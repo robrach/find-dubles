fa37e9d12b0c002752c77931f79b4756a3d2a357
Robert <robert.rachubinski@gmail.com>
2021-10-31 20:33:50 +0100

    def example(board) and some text fixes.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def generate_board(symbols):
 
 
 def print_board(board):
+    print(f'\nStatus of found dubles: {found_dubles}/{dubles}')
     column_counter = 0
     for key, value in board.items():
         print(f'{key}:{value}   ', end="")
@@ -25,10 +26,18 @@ def print_board(board):
             column_counter = 0
 
 
+def example(board):
+    first_choice = keys[0].lower()
+    first_choice_symbol = board[first_choice]
+    for key in board.keys():
+        if board[key] == first_choice_symbol and first_choice != key:
+            return f'Example: for FIRST CHOICE "{first_choice}" the SECOND CHOICE is "{key}".'
+
+
 def get_choice():
     print("")
-    first_choice = input('First choice: ')
-    second_choice = input('Second choice: ')
+    first_choice = input('FIRST CHOICE: ')
+    second_choice = input('SECOND CHOICE: ')
     if first_choice == second_choice:
         print("You can't input the same first and second choice! Input again.")
         get_choice()
@@ -47,8 +56,6 @@ def check_choice(choice):
     else:
         print("Bad choice...")
 
-    print(f'\nStatus: {found_dubles}/{dubles}')
-
 
 def modify_board(choice):
     board[choice[0]], board[choice[1]] = " ", " "
@@ -56,10 +63,13 @@ def modify_board(choice):
 
 if __name__ == '__main__':
     board = generate_board(symbols)
+
+    print('You must find all dubles.')
+    print(example(board))
+
     while found_dubles < dubles:
         print_board(board)
         choice = get_choice()
         check_choice(choice)
-
-
-
+    print_board(board)
+    print('\nCongratulations! You found all dubles!')
